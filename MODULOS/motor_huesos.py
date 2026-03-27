a0b46c656785be731b3490112d56c416783d6a41
CJPANTA <cjpanta1@gmail.com>
2026-03-27 13:32:14 -0500

    Update motor_huesos.py

--- MODULOS/motor_huesos.py
+++ MODULOS/motor_huesos.py
@@ -2,11 +2,10 @@ import streamlit as st
 import os
 import base64
 
-# Ruta raíz (donde está streamlit_app.py y tus logos)
+# Ruta raíz
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 def cargar_imagen_raiz(nombre_archivo):
-    """Carga imágenes (logos) directamente desde la raíz del proyecto"""
     ruta = os.path.join(BASE_DIR, nombre_archivo)
     if os.path.exists(ruta):
         with open(ruta, "rb") as f:
