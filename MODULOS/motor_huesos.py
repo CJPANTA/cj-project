e4f4fc48786c49dc2c9df36d319f75a822994ef2
CJPANTA <cjpanta1@gmail.com>
2026-03-27 15:10:26 -0500

    Update motor_huesos.py

--- MODULOS/motor_huesos.py
+++ MODULOS/motor_huesos.py
@@ -12,10 +12,3 @@ def cargar_imagen_raiz(nombre_archivo):
         ext = nombre_archivo.split('.')[-1]
         return f"data:image/{ext};base64,{base64.b64encode(data).decode()}"
     return None
-
-def listar_ciclos_reales():
-    ruta_carrion = os.path.join(BASE_DIR, "BASE_DATOS", "01_CARRION")
-    if os.path.exists(ruta_carrion):
-        # Ordenamos para que salga 01, 02, 03...
-        return sorted([d for d in os.listdir(ruta_carrion) if os.path.isdir(os.path.join(ruta_carrion, d))])
-    return []
