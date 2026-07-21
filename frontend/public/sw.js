dc54998409b47d0833aed4a98530691c305e6353
Jorge Luis Chiroque <cjpanta1@gmail.com>
2026-07-21 00:28:30 -0500

    Fix: Corregir vercel.json y sw.js para resolver error de login

---
+++ frontend/public/sw.js
@@ -0,0 +1,12 @@
+// Este es un Service Worker básico para tu PWA
+self.addEventListener('install', event => {
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
